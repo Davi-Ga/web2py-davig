d7a8087901ea1c3dddd434861fb96309ffb998ae
Davi-Ga <davigaldinoky@gmail.com>
2022-03-27 01:30:11 -0300

    aa

--- entrada.sh
+++ entrada.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# users can overwrite UWSGI_OPTIONS
 if [ "$UWSGI_OPTIONS" == '' ]; then
   UWSGI_OPTIONS='--master --thunder-lock --enable-threads'
 fi
+
+selectVersion() {
+  if [ "$WEB2PY_VERSION" != '' ]; then
+    git checkout $WEB2PY_VERSION
+  fi
+}
+
